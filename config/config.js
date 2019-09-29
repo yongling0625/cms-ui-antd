66acdf4b40315d19dbbbfcc6b824562f356e5bce
zhuyongling <79001833@qq.com>
2019-09-29 14:31:55 +0800

    first commit

---
+++ config/config.js
@@ -0,0 +1,27 @@
+// ref: https://umijs.org/config/
+export default {
+  treeShaking: true,
+  plugins: [
+    // ref: https://umijs.org/plugin/umi-plugin-react.html
+    ['umi-plugin-react', {
+      antd: true,
+      dva: true,
+      dynamicImport: { webpackChunkName: true },
+      title: 'cms-ui-antd',
+      dll: true,
+      locale: {
+        enable: true,
+        default: 'en-US',
+      },
+      routes: {
+        exclude: [
+          /models\//,
+          /services\//,
+          /model\.(t|j)sx?$/,
+          /service\.(t|j)sx?$/,
+          /components\//,
+        ],
+      },
+    }],
+  ],
+}
---
+++ config/proxy.config.js
@@ -0,0 +1,10 @@
+const env = {
+  mock: 'http://localhost:8000',
+  local: 'http://localhost:8112',
+  dev: 'http://api-dpc-dev.earth-aws-us.xpaas.lenovo.com',
+  sit: 'http://api-dpc-sit.earth-aws-us.xpaas.lenovo.com',
+  uat: 'http://api-dpc-uat.earth-aws-us.xpaas.lenovo.com',
+  prod: 'http://api-dpc-prod.earth-aws-us.xpaas.lenovo.com',
+};
+
+export default env;
